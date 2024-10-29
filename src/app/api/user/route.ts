cca064782c1744b9c41a61683abc7613725b698a
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-10-29 14:45:00 +0500

    created todos CRUD Route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,77 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+interface Item {
+  id: number;
+  name?: string;
+}
+
+const arr: Item[] = [];
+
+// Create
+export async function POST(request: NextRequest) {
   try {
-    console.log(request.method);
-    return NextResponse.json({ user: "Muhammad Farooq" }, { status: 200 });
+    const body: Item = await request.json();
+    arr.push(body);
+    return NextResponse.json(
+      { message: "Item added", item: body },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Read
+export async function GET() {
+  try {
+    return NextResponse.json(arr, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Update
+export async function PUT(request: NextRequest) {
+  try {
+    const body: Item = await request.json();
+    const index = arr.findIndex((item) => item.id === body.id);
+    if (index !== -1) {
+      arr[index] = body;
+      return NextResponse.json(
+        { message: "Item updated", item: body },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Delete
+export async function DELETE(request: NextRequest) {
+  try {
+    const body: { id: number } = await request.json();
+    const index = arr.findIndex((item) => item.id === body.id);
+    if (index !== -1) {
+      arr.splice(index, 1);
+      return NextResponse.json({ message: "Item deleted" }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
   } catch (error) {
     console.error("Error:", error);
     return NextResponse.json(
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,4 @@
-"use client";
-
-import { useEffect } from "react";
-
 export default function Home() {
-  useEffect(() => {
-    fetch("/api/user")
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  }, []);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-6xl">Hello World</h1>
